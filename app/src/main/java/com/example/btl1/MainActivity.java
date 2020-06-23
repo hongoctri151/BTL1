e38063c9d235e7bfa1a5ff3e8dd2e40e68abb483
hoangvutinh <hoangvutinh1@gmail.com>
2020-06-23 19:26:07 +0700

    Xac nhan username,password MainActivity

--- app/src/main/java/com/example/btl1/MainActivity.java
+++ app/src/main/java/com/example/btl1/MainActivity.java
@@ -5,13 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 //giao dien ban dau
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnLogin;
+    private String checkUserName="hvtinh1";
+    private String checkPassword="123456";
+    private EditText edPassword;
+    private EditText edUsername;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin=(Button) findViewById(R.id.btnLogin);
+        edPassword=(EditText) findViewById(R.id.password);
+        edUsername=(EditText)findViewById(R.id.username);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edUsername.getText().toString().equals(checkUserName)) {
+                    if (edPassword.getText().toString().equals(checkPassword)) {
+                        Intent intent=new Intent(MainActivity.this,MainMenu.class);
+                        startActivity(intent);
+
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Bạn đã nhập sai mật khẩu", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"Người dùng không tồn tại",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public void SignUp(View view) {
@@ -20,10 +52,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void Login(View view) {
-        Intent mainMenu=new Intent(this,MainMenu.class);//trang account_menu
-        startActivity(mainMenu);
-    }
+
     public void Cook(View view) {
         Intent mainMenu=new Intent(this,cook.class);//trang account_menu
         startActivity(mainMenu);
